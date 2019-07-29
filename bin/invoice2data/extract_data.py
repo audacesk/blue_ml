327347c82b8835a516965be4ab036e14c16d2d7e
JVQ <jv.quilichini@gmail.com>
2019-07-29 17:07:22 +0200

    Format json

--- bin/invoice2data/extract_data.py
+++ bin/invoice2data/extract_data.py
@@ -19,9 +19,7 @@ if __name__ == '__main__':
 
     output_data = extract_data(args.invoice_path, templates=templates)
 
-    # make the 'date': datetime object working please
-    date_time = output_data['date'].strftime('%d/%m/%Y')
+    date_time = output_data['date'].strftime('%Y-%m-%d')
     output_data['date'] = date_time
     # then print the formatted JSON data output
-    # print(json.dumps(json_data, indent=2))
-    print(output_data)
+    print(json.dumps(output_data, indent=2))
